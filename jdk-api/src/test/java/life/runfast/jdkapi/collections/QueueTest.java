c90819743bccddaf04657c03d8356479e7e351ec
justatempa <3238785005@qq.com>
2023-05-24 09:20:58 +0800

    jdk-api project init

---
+++ jdk-api/src/test/java/life/runfast/jdkapi/collections/QueueTest.java
@@ -0,0 +1,19 @@
+package life.runfast.jdkapi.collections;
+
+import org.junit.Test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+
+/**
+ * @author justatempa
+ * @description
+ * @datetime 2023/5/24 8:53
+ */
+public class QueueTest {
+
+   @Test
+    public void api() {
+       ArrayBlockingQueue<Integer> blockingDeque = new ArrayBlockingQueue<Integer>(10);
+   }
+}
